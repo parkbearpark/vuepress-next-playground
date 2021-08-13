3ee47b3a2ed85dae26cfd83c01532310750b8406
yuma <yumai6205@gmail.com>
2021-08-13 17:08:34 +0900

    sidebar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,12 +1,47 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
+import fs from 'fs'
+
+const dirpath = './docs'
+const ignoreDir = ['.vuepress']
+const dirs = fs.readdirSync(dirpath).filter(
+  f => {
+    if (ignoreDir.includes(f)) {
+      return false
+    }
+
+    const tmp = `${dirpath}/${f}`
+    return fs.existsSync(tmp) && fs.statSync(tmp).isDirectory()
+  }
+)
+
+const dirList = dirs.map(
+  dir => {
+    return {
+      text: dir,
+      title: dir,
+      children: fs.readdirSync(`${dirpath}/${dir}`).map(
+        child => {
+          const childName = child.replace('.md', '')
+          return {
+            text: childName,
+            link: `/${dir}/${childName}.html`,
+          }
+        }
+      )
+    }
+  }
+)
+const sidebarList = dirList
 
 export default defineUserConfig<DefaultThemeOptions>({
-  lang: 'en-US',
+  lang: 'ja',
   title: 'Hello VuePress',
   description: 'Just playing around',
-
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
-  },
+    docsDir: 'docs',
+    sidebarDepth: 3,
+    sidebar: sidebarList
+  }
 })
